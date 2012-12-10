5f517e16074f1bec17f5ace5dbaebe703b5155e9
nozomi <nozomisumiya@gmail.com>
2012-12-09 19:35:42 -0800

    add basic view and templates

--- windowshop/myshop/views.py
+++ windowshop/myshop/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+"""MyShop view functions"""
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def myshop(request):
+
+    return render_to_response('home.html',
+                              {},
+                              context_instance=RequestContext(request))
