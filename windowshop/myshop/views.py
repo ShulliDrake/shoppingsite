42785338902b24c249254aeb21fdd12329d09953
nozomi <nozomisumiya@gmail.com>
2012-12-16 13:05:44 -0800

    add some styles and pagination

--- windowshop/myshop/views.py
+++ windowshop/myshop/views.py
@@ -6,19 +6,46 @@ from django.template import RequestContext
 import requests
 
 
-def myshop(request):
+def myshop(request, page=1):
 
+    api_key = settings.API_KEY
     base_api = settings.API_PATH
-    api = base_api % ("JP", "cute+cat+goods")  # county code, query
+    items_per_page = settings.API_ITEMS_PER_PAGE
+    start_index = items_per_page * int(page)
+    api = base_api % ("US", "preserved+flower", start_index)  # county code, query
 
+
+    print api
     r = requests.get(api)
     products = r.json.get('items')
+    total_items = r.json.get('totalItems')
+    items_per_page = r.json.get('itemsPerPage')
 
-    for p in products:
-        description = p.get('product').get('description')
-        description = description[:200] + '...'  if len(description) > 200 else description
-        p['product']['description'] = description
+    pagination = _get_pagination(total_items, start_index, items_per_page)
+    print pagination
+
+    if products:
+        for p in products:
+            description = p.get('product').get('description')
+            description = description[:200] + '...'  if len(description) > 200 else description
+            p['product']['description'] = description
 
     return render_to_response('home.html',
-                              {'products': products},
+                              {'products': products,
+                               'pagination': pagination,
+                               'currentPage': int(page)},
                               context_instance=RequestContext(request))
+
+
+# Helper functions
+def _get_pagination(totalItems, startIndex, itemsPerPage):
+    """
+    """
+
+    if totalItems <= itemsPerPage:
+        return None
+
+    # calculate the number of pages to show
+    numOfPages = totalItems / itemsPerPage
+
+    return range(1, min(16, numOfPages))
--- windowshop/windowshop/urls.py
+++ windowshop/windowshop/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'myshop.views.myshop')
+    url(r'^$', 'myshop.views.myshop'),
+    url(r'^(?P<page>\d+)$', 'myshop.views.myshop')
 )
