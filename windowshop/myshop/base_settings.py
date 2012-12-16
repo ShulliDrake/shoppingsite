e5e394a658f1a224b36b3624453198d89ff1de1e
nozomi <nozomisumiya@gmail.com>
2012-12-16 13:46:35 -0800

    -style changes -pagination at top -24 items per page

--- windowshop/myshop/base_settings.py
+++ windowshop/myshop/base_settings.py
@@ -1,8 +1,8 @@
 # My shop configurations
 API_KEY = "AIzaSyA4tqHma0vcR-VC7iPyo3il69-gF4urh7s"
 
-API_PATH = "https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyA4tqHma0vcR-VC7iPyo3il69-gF4urh7s&country=%s&q=%s&startIndex=%s&alt=json"
+API_PATH = "https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyA4tqHma0vcR-VC7iPyo3il69-gF4urh7s&country=%s&q=%s&startIndex=%s&maxResults=%s&alt=json"
 
 API_CROWD = "&crowdBy=accountId:1"
 
-API_ITEMS_PER_PAGE = 25
+API_ITEMS_PER_PAGE = 24
--- windowshop/myshop/views.py
+++ windowshop/myshop/views.py
@@ -12,24 +12,23 @@ def myshop(request, page=1):
     base_api = settings.API_PATH
     items_per_page = settings.API_ITEMS_PER_PAGE
     start_index = items_per_page * int(page)
-    api = base_api % ("US", "preserved+flower", start_index)  # county code, query
+    api = base_api % ("JP", "preserved+flower", start_index, items_per_page)  # county code, query, start index, max results
 
 
-    print api
     r = requests.get(api)
     products = r.json.get('items')
     total_items = r.json.get('totalItems')
     items_per_page = r.json.get('itemsPerPage')
 
     pagination = _get_pagination(total_items, start_index, items_per_page)
-    print pagination
 
+    """
     if products:
         for p in products:
             description = p.get('product').get('description')
             description = description[:200] + '...'  if len(description) > 200 else description
             p['product']['description'] = description
-
+            """
     return render_to_response('home.html',
                               {'products': products,
                                'pagination': pagination,
