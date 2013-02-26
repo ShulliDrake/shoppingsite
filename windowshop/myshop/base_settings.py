bcc22c65b2defbd1ae1712dcd6922abf7ba8c165
nozomi <nozomisumiya@gmail.com>
2013-02-26 15:29:40 -0800

    use OrderedDict for browse list configs

--- windowshop/myshop/base_settings.py
+++ windowshop/myshop/base_settings.py
@@ -1,4 +1,5 @@
 # My shop configurations
+from collections import OrderedDict
 
 # Google Shopping API
 API_KEY = "AIzaSyA4tqHma0vcR-VC7iPyo3il69-gF4urh7s"
@@ -10,32 +11,32 @@ API_CROWD = "&crowdBy=accountId:1"
 API_ITEMS_PER_PAGE = 24
 
 # Browse Categories
-BROWSE_ITEMS = {
-    "Beauty": {
-        "Makeup": ["Foundation", "Lipstick", "Lip Gloss", "Eyeshadow", "Mascara", "False Eyelashes"],
-        "Skincare": ["Eye cream", "Moisturizer", "Toner", "Sunscreen"]
-        },
-    "Apparel": {
-        "Dresses": ["Cocktail Dress", "Prom", "Bridesmaid", "Wedding Dress"],
-        "Activewear": ["Yoga Tops", "Yoga Pants", "Running Jacket", "Running Shorts", "Tennis Skirt"],
-        "Men": ["Suits", "Tie", "Tie Pin", "Dress Shirt"]
-        },
-    "Shoes": {
-        "Dress": ["Pumps", "Mary Janes", "Booties", "Loafers"],
-        "Casual": ["Flip Flops", "Sneakers", "Sandals", "Slippers", "Espadrilles"]
-        },
-    "Electronics": {
-        "Computers": ["iMac", "MacBook", "Chromebook", "Ultrabooks", "Microsoft Surface"],
-        "Phones": ["Android", "iPhone", "Phone Case", "Phone Charger"],
-        "Electronics": ["HDTV", "Digital Camera", "Home Theater", "Bluetooth Headset"]
-        },
-    "Games": {
-        "Consoles": ["Xbox 360", "PS3", "Wii", "WiiU", "PS Vista", "Nintendo 3DS"],
-        "Video Games": ["Xbox Games", "PS Games", "DS Games", "Super Mario"],
-        "Non-Video Games": ["Board Games", "Puzzle Games", "Word Games", "Chess"],
-        },
-    "Flowers": {
-        "Flowers": ["Tulips", "Roses", "Lilies", "Mini Cactus", "Marimo Aquarium", "Preserved Flowers"],
-        "Special Occasions": ["Red Roses", "Anniversary Bouquet", "Corsages"]
-        }
-    }
+BROWSE_ITEMS = OrderedDict([
+        ("Electronics", {
+                "Computers": ["iMac", "MacBook", "Chromebook", "Ultrabooks", "Microsoft Surface"],
+                "Phones": ["Android", "iPhone", "Phone Case", "Phone Charger"],
+                "Electronics": ["HDTV", "Digital Camera", "Home Theater", "Bluetooth Headset"]
+                }),
+        ("Games", {
+                "Consoles": ["Xbox 360", "PS3", "Wii", "WiiU", "PS Vista", "Nintendo 3DS"],
+                "Video Games": ["Xbox Games", "PS Games", "DS Games", "Super Mario"],
+                "Non-Video Games": ["Board Games", "Puzzle Games", "Word Games", "Chess"],
+                }),
+        ("Beauty", {
+                "Makeup": ["Foundation", "Lipstick", "Lip Gloss", "Eyeshadow", "Mascara", "False Eyelashes"],
+                "Skincare": ["Eye cream", "Moisturizer", "Toner", "Sunscreen"]
+                }),
+        ("Flowers", {
+                "Flowers": ["Tulips", "Roses", "Lilies", "Mini Cactus", "Marimo Aquarium", "Preserved Flowers"],
+                "Special Occasions": ["Red Roses", "Anniversary Bouquet", "Corsages"]
+                }),
+        ("Shoes", {
+                "Dress": ["Pumps", "Mary Janes", "Booties", "Loafers"],
+                "Casual": ["Flip Flops", "Sneakers", "Sandals", "Slippers", "Espadrilles"]
+                }),
+        ("Apparel", {
+                "Activewear": ["Yoga Tops", "Yoga Pants", "Running Jacket", "Running Shorts", "Tennis Skirt"],
+                "Men": ["Suits", "Tie", "Tie Pin", "Dress Shirt"],
+                "Dresses": ["Cocktail Dress", "Prom", "Bridesmaid", "Wedding Dress"]
+                }),
+        ])
